79ede5263aba2000b6e7bad0d38d002b10e80a7a
Carlos Barrientos <cgbarr89@gmail.com>
2021-08-07 14:37:33 -0500

    COMPLETEd

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# google-book-app
-Google Book Search App
+# Book-Search-Engine
+Find and retrieve information on some of your favorite books using the Google Books API Search Application! Created with a focus on using the MERN stack.
+
+[Link to GitHub Repository](https://github.com/WitnessMyHands/Book-Search-Engine)
+
+[Link to Heroku Deployed Application](https://wmh-book-search-engine.herokuapp.com/)
+
+## Table Of Contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Gallery](#Gallery)
+- [Questions](#Questions)
+
+## Description
+This fully function application allows users to search for books using the Google Books API. The application was built using MongoDB for databasing, Express/Node.js for server-side and API & ReactJS for front end deployment. Styled using Bootstrap.
+
+## Installation
+- Clone the Repository using Terminal: `git clone git@github.com:WitnessMyHands/Book-Search-Engine.git`
+- Change directory, "cd" into the Local Folder
+- Install Dependencies with: `npm install`
+- To run the Application you will need to run: `npm start`
+- When complete, the web application should open in a new window. If browser window does not open, navigate to - https://localhost:3000 
+
+## Gallery
+[![Watch the MERN Video](./mern-screen.png)](https://www.youtube.com/watch?v=EqC5_CKi9tk)
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Questions
+*Please contact WitnessMyHands via GitHub with any questions or concerns.*
+
+- https://github.com/WitnessMyHands
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,68 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user){
+                const userData = await User.findOne({_id: context.user._id})
+                .select('-__v -password')
+          
+              return userData;
+            }
+            throw new AuthenticationError('Not Logged In');
+        }
+    },
+    Mutation: {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email})
+
+            if (!user){
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return  { token, user };
+        },
+        saveBook: async (parent, { dataBook }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: dataBook } },
+                { new: true }
+              );
+          
+              return updatedUser;
+            }
+          
+            throw new AuthenticationError('You need to be Logged In!');
+        },
+        removeBook: async(parent, {bookId}, context) =>{
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                  );
+    
+              return updatedUser;
+            }
+            throw new AuthenticationError('You need to be Logged In!');
+        }
+    }
+}
+
+module.exports = resolvers;
